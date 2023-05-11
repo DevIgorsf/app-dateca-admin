268cf0d3ebb20f212a5559484b87fa5aaa6eef6f
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2023-05-10 22:19:56 -0300

    app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 
 const routes: Routes = [
-
-]
+  {path: '', component: SidebarComponent, pathMatch: 'full'}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
