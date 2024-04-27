4331ce3d2255e0cd09d66ae5e94672dc486563fc
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2024-04-27 19:59:08 -0300

    apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  ApiUrl: 'http://localhost:8080'
+  ApiUrl: 'https://app-dateca-backend-production.up.railway.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  ApiUrl: 'http://localhost:8080'
+  ApiUrl: 'https://app-dateca-backend-production.up.railway.app'
 };
 
 /*
